69fb2d0b2cba80a25d04356fa6badd629baf58b9
breezeyboy <jay_breeze@hotmail.co.uk>
2013-03-10 10:56:48 +0000

    Removed unused code....

--- src/com/breezeyboy/plookup/main.java
+++ src/com/breezeyboy/plookup/main.java
@@ -1,9 +1,7 @@
 package com.breezeyboy.plookup;
 
-import java.io.File;
 import java.io.IOException;
 
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -13,8 +11,6 @@ import com.breezeyboy.plookup.metrics.Metrics;
 public class main extends JavaPlugin{
 public PluginDescriptionFile pdfile = this.getDescription();
 public String prefix = "[PlayerLookup] ";
-File dat = new File("plugins/plookup/config.yml");
-YamlConfiguration data = YamlConfiguration.loadConfiguration(new File("plugins/plookup/config.yml"));
 
 public void onEnable(){
 System.out.println(prefix + "Plugin loaded successfully. (v1.1)");
